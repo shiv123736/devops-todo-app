b3516d7fc18d7c1dbc44eb97fd56f386a1ae17e2
shiv123736 <dharmasya0@gmail.com>
2026-01-11 15:26:15 +0530

    Test: Render auto Deploy from repo

--- src/route/homeRoute.ts
+++ src/route/homeRoute.ts
@@ -4,7 +4,7 @@ const router = Router();
 
 // Home Route GET /api
 router.get("/", (_req, res) => {
-  res.json({ message: "Welcome to Home Page!" });
+  res.json({ message: "Welcome to Updated Home Page!" });
 });
 
 export default router;

265fc3073903f0dcd3ab6909d885b735f4137473
shiv123736 <dharmasya0@gmail.com>
2026-01-14 20:36:47 +0530

    error: lint errors and tsconfig types

--- src/math.test.ts
+++ src/math.test.ts
@@ -1,5 +1,5 @@
-describe('Chaos Test', () => {
-  it('should fail purposefully', () => {
-    expect(true).toBe(false);
+describe('Math Test', () => {
+  it('should add numbers correctly', () => {
+    expect(1 - 1).toBe(2);
   });
 });
